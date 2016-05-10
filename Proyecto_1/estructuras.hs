3af624d064a388376dc0281b29d4c89479588b83
Pablo Maldonado <prmm95@gmail.com>
2016-05-10 17:53:13 -0400

    Se agregaron los shows de las operaciones: Or, And, Impl, Equiv, NoEquix, Equa y Sust (simple)

--- Proyecto_1/estructuras.hs
+++ Proyecto_1/estructuras.hs
@@ -92,6 +92,7 @@ false =  Bool "false"
 (/\) :: Term -> Term -> Term
 (/\) t1 t2 = And t1 t2
 
+infixr 8 ==>
 (==>) :: Term -> Term -> Term
 (==>) t1 t2 = Impl t1 t2
 
@@ -101,9 +102,11 @@ false =  Bool "false"
 (!<==>) :: Term -> Term -> Term
 (!<==>) t1 t2 = NoEquiv t1 t2
 
+infix 2 ===
 (===) :: Term -> Term -> Equation
 (===) t1 t2 = Equa t1 t2
 
+infix 2 =:
 (=:) :: Term -> Term -> Sust
 (=:) t1 t2 = Sustitution t1 t2
 
@@ -116,7 +119,39 @@ with::
 using::
 lambda::-}
 
+
 showTerm :: Term -> String
 showTerm (Var x) = x
 showTerm (Bool x) = x
+showTerm (Or (Var x) (Var y)) = showTerm(Var x) ++ "\\/" ++ showTerm(Var y)
+showTerm (Or (Var x) t) = showTerm(Var x) ++ " \\/ (" ++ showTerm(t) ++ ")"
+showTerm (Or t (Var x)) = "(" ++ showTerm(t) ++ ")" ++ " \\/ " ++ showTerm(Var x)
+showTerm (Or t1 t2) = "(" ++ showTerm t1 ++ ") \\/ (" ++ showTerm t2 ++ ")"
+showTerm (And (Var x) (Var y)) = showTerm(Var x) ++ "/\\" ++ showTerm(Var y)
+showTerm (And (Var x) t) = showTerm(Var x) ++ " /\\ (" ++ showTerm(t) ++ ")"
+showTerm (And t (Var x)) = "(" ++ showTerm(t) ++ ")" ++ " /\\ " ++ showTerm(Var x)
+showTerm (And t1 t2) = "(" ++ showTerm t1 ++ ") /\\ (" ++ showTerm t2 ++ ")"
+showTerm (Impl (Var x) (Var y)) = showTerm(Var x) ++ "==>" ++ showTerm(Var y)
+showTerm (Impl (Var x) t) = showTerm(Var x) ++ " ==> (" ++ showTerm(t) ++ ")"
+showTerm (Impl t (Var x)) = "(" ++ showTerm(t) ++ ")" ++ " ==> " ++ showTerm(Var x)
+showTerm (Impl t1 t2) = "(" ++ showTerm t1 ++ ") ==> (" ++ showTerm t2 ++ ")"
+showTerm (Equiv (Var x) (Var y)) = showTerm(Var x) ++ "<==>" ++ showTerm(Var y)
+showTerm (Equiv (Var x) t) = showTerm(Var x) ++ " <==> (" ++ showTerm(t) ++ ")"
+showTerm (Equiv t (Var x)) = "(" ++ showTerm(t) ++ ")" ++ " <==> " ++ showTerm(Var x)
+showTerm (Equiv t1 t2) = "(" ++ showTerm t1 ++ ") <==> (" ++ showTerm t2 ++ ")"
+showTerm (NoEquiv (Var x) (Var y)) = showTerm(Var x) ++ "!<==>" ++ showTerm(Var y)
+showTerm (NoEquiv (Var x) t) = showTerm(Var x) ++ " !<==> (" ++ showTerm(t) ++ ")"
+showTerm (NoEquiv t (Var x)) = "(" ++ showTerm(t) ++ ")" ++ " !<==> " ++ showTerm(Var x)
+showTerm (NoEquiv t1 t2) = "(" ++ showTerm t1 ++ ") !<==> (" ++ showTerm t2 ++ ")"
 instance Show Term where show = showTerm
+
+showEquation :: Equation -> String
+showEquation (Equa t1 t2) = "(" ++ showTerm t1 ++ ") === (" ++ showTerm t2 ++ ")"
+instance Show Equation where show = showEquation
+
+showSustitution :: Sust -> String
+showSustitution (Sustitution t1 t2) =  showTerm t1 ++ "=:" ++ showTerm t2
+instance Show Sust where show = showSustitution
+
+
+
