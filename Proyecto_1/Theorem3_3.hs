8b961bb5c0e1a7835a6d3f1211b41c27e2885ad2
alejandra <alejandra@cc.ac.labf.usb.ve>
2016-05-14 00:19:13 -0400

    Casos de prueba

--- Proyecto_1/Theorem3_3.hs
+++ Proyecto_1/Theorem3_3.hs
@@ -1,8 +1,10 @@
+import Funciones
+import Estructuras
 verify = let theorem = (p <==> p) <==> true === true in
          proof theorem
          >>=
          statement 3.3 with (p =: p) using lambda z (z <==> true)
          >>=
-         statement 3.3 with (true =: p) using lambda z (z)
+         statement 3.1 with (true =: p) using lambda z (z)
          >>=
          done theorem
