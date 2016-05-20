54ec974deee1d2f1cc820389f1fcf74fdb9189a4
Pablo Maldonado <prmm95@gmail.com>
2016-05-20 03:12:31 +0000

    Se arreglaron los shows para considerar los casos con Bool

--- Proyecto_1/Funciones.hs
+++ Proyecto_1/Funciones.hs
@@ -102,31 +102,65 @@ done (Equa t1 t2) term1 = do { if term1==t2 then putStrLn "Proof succesful." els
 
 
 showTerm :: Term -> String
+
 showTerm (Var x) = x
+
 showTerm (Bool x) = x
+
 showTerm (Not (Var x)) = "neg " ++ showTerm(Var x)
 showTerm (Not (Bool x)) = "neg " ++ showTerm (Bool x)
 showTerm (Not t) = "neg " ++ "(" ++  showTerm t ++ ")"
+
 showTerm (Or (Var x) (Var y)) = showTerm(Var x) ++ "\\/" ++ showTerm(Var y)
+showTerm (Or (Var x) (Bool y)) = showTerm(Var x) ++ "\\/" ++ showTerm(Bool y)
+showTerm (Or (Bool x) (Var y)) = showTerm(Bool x) ++ "\\/" ++ showTerm(Var y)
 showTerm (Or (Var x) t) = showTerm(Var x) ++ " \\/ (" ++ showTerm(t) ++ ")"
 showTerm (Or t (Var x)) = "(" ++ showTerm(t) ++ ")" ++ " \\/ " ++ showTerm(Var x)
+showTerm (Or (Bool x) (Bool y)) = showTerm(Bool x) ++ "\\/" ++ showTerm(Bool y)
+showTerm (Or (Bool x) t) = showTerm(Bool x) ++ " \\/ (" ++ showTerm(t) ++ ")"
+showTerm (Or t (Bool x)) = "(" ++ showTerm(t) ++ ")" ++ " \\/ " ++ showTerm(Bool x)
 showTerm (Or t1 t2) = "(" ++ showTerm t1 ++ ") \\/ (" ++ showTerm t2 ++ ")"
+
 showTerm (And (Var x) (Var y)) = showTerm(Var x) ++ "/\\" ++ showTerm(Var y)
+showTerm (And (Var x) (Bool y)) = showTerm(Var x) ++ "/\\" ++ showTerm(Bool y)
+showTerm (And (Bool x) (Var y)) = showTerm(Bool x) ++ "/\\" ++ showTerm(Var y)
 showTerm (And (Var x) t) = showTerm(Var x) ++ " /\\ (" ++ showTerm(t) ++ ")"
 showTerm (And t (Var x)) = "(" ++ showTerm(t) ++ ")" ++ " /\\ " ++ showTerm(Var x)
+showTerm (And (Bool x) (Bool y)) = showTerm(Bool x) ++ "/\\" ++ showTerm(Bool y)
+showTerm (And (Bool x) t) = showTerm(Bool x) ++ " /\\ (" ++ showTerm(t) ++ ")"
+showTerm (And t (Bool x)) = "(" ++ showTerm(t) ++ ")" ++ " /\\ " ++ showTerm(Bool x)
 showTerm (And t1 t2) = "(" ++ showTerm t1 ++ ") /\\ (" ++ showTerm t2 ++ ")"
+
 showTerm (Impl (Var x) (Var y)) = showTerm(Var x) ++ "==>" ++ showTerm(Var y)
+showTerm (Impl (Var x) (Bool y)) = showTerm(Var x) ++ "==>" ++ showTerm(Bool y)
+showTerm (Impl (Bool x) (Var y)) = showTerm(Bool x) ++ "==>" ++ showTerm(Var y)
 showTerm (Impl (Var x) t) = showTerm(Var x) ++ " ==> (" ++ showTerm(t) ++ ")"
 showTerm (Impl t (Var x)) = "(" ++ showTerm(t) ++ ")" ++ " ==> " ++ showTerm(Var x)
+showTerm (Impl (Bool x) (Bool y)) = showTerm(Bool x) ++ "==>" ++ showTerm(Bool y)
+showTerm (Impl (Bool x) t) = showTerm(Bool x) ++ " ==> (" ++ showTerm(t) ++ ")"
+showTerm (Impl t (Bool x)) = "(" ++ showTerm(t) ++ ")" ++ " ==> " ++ showTerm(Bool x)
 showTerm (Impl t1 t2) = "(" ++ showTerm t1 ++ ") ==> (" ++ showTerm t2 ++ ")"
+
 showTerm (Equiv (Var x) (Var y)) = showTerm(Var x) ++ "<==>" ++ showTerm(Var y)
+showTerm (Equiv (Var x) (Bool y)) = showTerm(Var x) ++ "<==>" ++ showTerm(Bool y)
+showTerm (Equiv (Bool x) (Var y)) = showTerm(Bool x) ++ "<==>" ++ showTerm(Var y)
 showTerm (Equiv (Var x) t) = showTerm(Var x) ++ " <==> (" ++ showTerm(t) ++ ")"
 showTerm (Equiv t (Var x)) = "(" ++ showTerm(t) ++ ")" ++ " <==> " ++ showTerm(Var x)
+showTerm (Equiv (Bool x) (Bool y)) = showTerm(Bool x) ++ "<==>" ++ showTerm(Bool y)
+showTerm (Equiv (Bool x) t) = showTerm(Bool x) ++ " <==> (" ++ showTerm(t) ++ ")"
+showTerm (Equiv t (Bool x)) = "(" ++ showTerm(t) ++ ")" ++ " <==> " ++ showTerm(Bool x)
 showTerm (Equiv t1 t2) = "(" ++ showTerm t1 ++ ") <==> (" ++ showTerm t2 ++ ")"
+
 showTerm (NoEquiv (Var x) (Var y)) = showTerm(Var x) ++ "!<==>" ++ showTerm(Var y)
+showTerm (NoEquiv (Var x) (Bool y)) = showTerm(Var x) ++ "!<==>" ++ showTerm(Bool y)
+showTerm (NoEquiv (Bool x) (Var y)) = showTerm(Bool x) ++ "!<==>" ++ showTerm(Var y)
 showTerm (NoEquiv (Var x) t) = showTerm(Var x) ++ " !<==> (" ++ showTerm(t) ++ ")"
 showTerm (NoEquiv t (Var x)) = "(" ++ showTerm(t) ++ ")" ++ " !<==> " ++ showTerm(Var x)
+showTerm (NoEquiv (Bool x) (Bool y)) = showTerm(Bool x) ++ "!<==>" ++ showTerm(Bool y)
+showTerm (NoEquiv (Bool x) t) = showTerm(Bool x) ++ " !<==> (" ++ showTerm(t) ++ ")"
+showTerm (NoEquiv t (Bool x)) = "(" ++ showTerm(t) ++ ")" ++ " !<==> " ++ showTerm(Bool x)
 showTerm (NoEquiv t1 t2) = "(" ++ showTerm t1 ++ ") !<==> (" ++ showTerm t2 ++ ")"
+
 instance Show Term where show = showTerm
 
 showEquation :: Equation -> String
