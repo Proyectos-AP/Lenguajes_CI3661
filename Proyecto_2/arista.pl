c9834916b688d62ffefa98645161d68818ec19c7
alejandra <alejandra@cc.ac.labf.usb.ve>
2016-06-09 23:19:37 -0400

    bien_etiquetado verifica si hay nodos o aristas repetidos

--- Proyecto_2/arista.pl
+++ Proyecto_2/arista.pl
@@ -11,15 +11,30 @@ arbol(nodo(X,[H|T])) :-
 	integer(X),
 	X > 0.
 
-bien_etiquetado(nodo(X,[])).
+lista([],[],[]).
+lista(arista(X,nodo(Y,A)),Ar,N):-
+	lista(A,R1,R2),
+	Ar = R1,
+	N =  [Y|R2].
 
+
+lista([H|T],A,N) :-
+	lista(H,A1,N1),
+	lista(T,A2,N2),
+	append(A1,A2,A),
+	append(N1,N2,N).
+
+bien_etiquetado(nodo(X,[])).
 bien_etiquetado(nodo(X,[H|T])) :-
-	buscarA(H,Arist,Nod,Lista_nodos,Lista_aristas),
+	buscarA(H,Arist,Nod),
 	Etiqueta is abs(X-Nod),
 	Etiqueta == Arist,
+	lista([H|T],A,N),
+	\+ member(Arist,A),
+	\+ member(X,N),
 	bien_etiquetado(nodo(X,T)).
 
-buscarA(arista(X,nodo(Y,A)),X,Y,Lista_nodos,Lista_aristas) :-
+buscarA(arista(X,nodo(Y,A)),X,Y) :-
 	bien_etiquetado(nodo(Y,A)).
 
 
