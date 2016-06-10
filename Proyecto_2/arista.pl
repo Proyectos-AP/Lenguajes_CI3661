8a61839552310b2a59c1b7d87cbcc12bbcb26416
alejandra <alejandra@cc.ac.labf.usb.ve>
2016-06-10 13:11:04 -0400

    Iniciando la implementacion de esqueleto

--- Proyecto_2/arista.pl
+++ Proyecto_2/arista.pl
@@ -14,7 +14,8 @@ arbol(nodo(X,[H|T])) :-
 lista([],[],[]).
 lista(arista(X,nodo(Y,A)),Ar,N):-
 	lista(A,R1,R2),
-	Ar = R1,
+	\+ member(X,R1),
+	Ar = [X|R1],
 	N =  [Y|R2].
 
 
@@ -32,9 +33,32 @@ bien_etiquetado(nodo(X,[H|T])) :-
 	Etiqueta is abs(X-Nod),
 	Etiqueta == Arist,
 	lista([H|T],A,N),
-	\+ member(Arist,A),
 	\+ member(X,N),
 	bien_etiquetado(nodo(X,T)).
 
 buscarA(arista(X,nodo(Y,A)),X,Y) :-
 	bien_etiquetado(nodo(Y,A)).
+
+
+generar(0,0,[]).
+generar(N,M,[M]):- 	
+	integer(N),
+	integer(M),
+	N==M.
+generar(N,M,L):-
+	integer(N),
+	integer(M),
+	N < M,
+	N1 is N+1,
+	generar(N1,M,L1),
+	L = [N|L1].
+
+esqueleto(N,R,[]) :- integer(R), R > 0,N==0. 
+esqueleto(N,R,L) :-
+	integer(N),
+	generar(1,N,ListGen),
+	member(X,ListGen),
+	X1 is N-X,
+	esqueleto(X1,R,L1),
+	append([[X]],L1,L).
+
