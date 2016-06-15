e870d4c70d7c47462aa0ddd3d4e5e3cd6b61ddc4
Pablo Maldonado <prmm95@gmail.com>
2016-06-15 00:54:25 -0400

    Se agrego estructura para la documentacion

--- Proyecto_2/arista.pl
+++ Proyecto_2/arista.pl
@@ -1,27 +1,60 @@
-%---------------------------------------------------------
-% Estructuras
-%---------------------------------------------------------
+/*
+ * Archivo: arista.pl
+ *
+ * Descripción: Implementación de predicados que permiten verificar 
+ * etiquetamientos de un árbol, generar esqueletos, etiquetarlos, e 
+ * imprimir dicha estructura de forma legible para el usuario.
+ *
+ * Autores:
+ *     - Alejandra Cordero / 12-10645
+ *     - Pablo Maldonado / 12-10561
+ *
+ * Última fecha de modificación: 16/06/2016
+ *
+*/
 
+
+%------------------------------------------------------------------------------%
+%                        DEFINICIÓN DE ESTRUCTURAS                             %
+%------------------------------------------------------------------------------%
+
+%%
+%%
 arista(X,A) :- 
 	integer(X),
 	X > 0,
 	arbol(A).
 
+%------------------------------------------------------------------------------%
+
+%%
+%%
 arbol(nodo(X,[])) :- 
 	integer(X),
 	X > 0.
-	
+
+%%
+%%	
 arbol(nodo(X,_)) :-
 	integer(X),
 	X > 0.
 
+%------------------------------------------------------------------------------%
+
+%%
+%%
 lista([],[],[]).
+
+%%
+%%
 lista(arista(X,nodo(Y,A)),Ar,N):-
 	lista(A,R1,R2),
 	\+ member(X,R1),
 	Ar = [X|R1],
 	N =  [Y|R2].
 
+%%
+%%
 lista([H|T],A,N) :-
 	lista(H,A1,N1),
 	lista(T,A2,N2),
@@ -30,11 +63,16 @@ lista([H|T],A,N) :-
 	append(N1,N2,N),
 	is_set(N).
 
-%---------------------------------------------------------
-% Predicado para verificar si un arbol esta bien etiquetado
-%---------------------------------------------------------
+%------------------------------------------------------------------------------%
+%              VERIFICACIÓN DE BUEN ETIQUETAMIENDO DE UN ÁRBOL                 %
+%------------------------------------------------------------------------------%
 
+%%
+%%
 bienEtiquetado(nodo(1,[])).
+
+%%
+%%
 bienEtiquetado(nodo(X,[H|T])) :-
 	bienEtiquetadoR(nodo(X,[H|T]),A,N),
 	append(N,[X],NR),
@@ -43,7 +81,14 @@ bienEtiquetado(nodo(X,[H|T])) :-
 	etiquetas_validas(A,LA),
 	etiquetas_validas(NR,LN).
 
+%------------------------------------------------------------------------------%
+
+%%
+%%
 bienEtiquetadoR(nodo(_,[]),_,_).
+
+%%
+%%
 bienEtiquetadoR(nodo(X,[H|T]),A,N) :-
 	buscarA(H,Arist,Nod),
 	Etiqueta is abs(X-Nod),
@@ -52,43 +97,74 @@ bienEtiquetadoR(nodo(X,[H|T]),A,N) :-
 	\+ member(X,N),
 	bienEtiquetadoR(nodo(X,T),_,_).
 
+%------------------------------------------------------------------------------%
+
+%%
+%%
 buscarA(arista(X,nodo(Y,A)),X,Y) :-
 	bienEtiquetadoR(nodo(Y,A),_,_).
 
+%%
+%%
 etiquetas_validas([],_).
+
+%%
+%%
 etiquetas_validas([H|T],UpperLimit) :-
 	H =< UpperLimit,
 	etiquetas_validas(T,UpperLimit).
 
-%---------------------------------------------------------
-% Predicados auxiliares para el esqueleto
-%---------------------------------------------------------
-% Predicado que suma los elementos de una lista 
-%                (Se puede eliminar)
-%---------------------------------------------------------
+%------------------------------------------------------------------------------%
+%                    PREDICADOS AUXILIARES DE ESQUELETO 
+%------------------------------------------------------------------------------%
+%                     SUMAR LOS ELEMENTOS DE UNA LISTA               
+%------------------------------------------------------------------------------%
 
+%%
+%%
 suma([],0).
+
+%%
+%%
 suma([H|T],Sumalista) :-
 	integer(H),
 	suma(T,R),
 	Sumalista is H + R.
 
+%------------------------------------------------------------------------------%
+
+%%
+%%
 sumarLista([[]],0).
+
+%%
+%%
 sumarLista([],0).
+
+%%
+%%
 sumarLista([H|T],N):-
 	suma(H,N1),
 	sumarLista(T,N2),
 	N is N1+N2.
-%---------------------------------------------------------
-% Predicado que genera una lista dado un rango
-%---------------------------------------------------------
 
+%------------------------------------------------------------------------------%
+%                      GENERAR UNA LISTA DADO UN RANGO                         %
+%------------------------------------------------------------------------------%
+
+%%
+%%
 generar(0,0,[]).
+
+%%
+%%
 generar(N,M,[M]):- 	
 	integer(N),
 	integer(M),
 	N==M.
 
+%%
+%%
 generar(N,M,L):-
 	integer(N),
 	integer(M),
@@ -97,11 +173,16 @@ generar(N,M,L):-
 	generar(N1,M,L1),
 	L = [N|L1].
 
-%---------------------------------------------------------
-%      Predicado generador de esqueletos
-%---------------------------------------------------------
+%------------------------------------------------------------------------------%
+%                         GENERADOR DE ESQUELETOS                              %
+%------------------------------------------------------------------------------%
 
+%%
+%%
 generador(N,NHijos,_,[]) :- integer(N), N==0, NHijos==0 ,!.
+
+%%
+%%
 generador(N,NHijos,ListaG,L) :- 
 	integer(N), 
 	N==0,
@@ -109,6 +190,8 @@ generador(N,NHijos,ListaG,L) :-
 	generar_esqueleto(N,NHijos,ListaG,_,_,L1),
 	L = [L1],!.
 
+%%
+%%
 generador(N,NHijos,ListaG,L) :-
 	integer(N),
 	integer(NHijos),
@@ -118,12 +201,20 @@ generador(N,NHijos,ListaG,L) :-
 	generador(X1,Suma,ListaG,L2),
 	append([L1],L2,L).
 
-%---------------------------------------------------------
-%    Predicado que genera la listas del esqueleto
-%---------------------------------------------------------
+%------------------------------------------------------------------------------%
+%                  GENERADOR DE LAS LISTAS DEL ESQUELETO                       %
+%------------------------------------------------------------------------------%
 
+%%
+%%
 generar_esqueleto(_,NHijos,_,0,0,[]):- integer(NHijos), NHijos==0,!.
+
+%%
+%%
 generar_esqueleto(_,_,[],0,0,[]).
+
+%%
+%%
 generar_esqueleto(N,NHijos,ListaG,0,0,Esqueleto):- 
 	integer(N),
 	integer(NHijos), 
@@ -133,6 +224,8 @@ generar_esqueleto(N,NHijos,ListaG,0,0,Esqueleto):-
 	generar_esqueleto(N,X1,ListaG,_,_,L1),
 	append([0],L1,Esqueleto).
 
+%%
+%%
 generar_esqueleto(N,NHijos,ListaG,Minimo,Suma,Esqueleto) :-
 	integer(N),
 	integer(NHijos),
@@ -148,11 +241,16 @@ generar_esqueleto(N,NHijos,ListaG,Minimo,Suma,Esqueleto) :-
 	Suma is Suma2 + X,
 	append([X],L1,Esqueleto).
 
-%---------------------------------------------------------
-%                Predicado esqueleto
-%---------------------------------------------------------
+%------------------------------------------------------------------------------%
+%                                 ESQUELETO                                    %
+%------------------------------------------------------------------------------%
 
+%%
+%%
 esqueleto(N,R,[]) :- integer(R), R > 0, N==0. 
+
+%%
+%%
 esqueleto(N,R,L) :-
 	integer(N),
 	integer(R),
@@ -166,11 +264,16 @@ esqueleto(N,R,L) :-
 	generador(XN,X,ListGen,LN),
 	append([[X]],LN,L).
 
-%---------------------------------------------------------
-%                Predicado etiquetable
-%---------------------------------------------------------
+%------------------------------------------------------------------------------%
+%                                 ETIQUETABLE                                  %
+%------------------------------------------------------------------------------%
 
+%%
+%%
 etiquetamiento([[]],[]).
+
+%%
+%%
 etiquetamiento([H|T],Arbol) :- 
 	suma(H,Result),
 	sumarLista([H|T],N),
@@ -183,20 +286,30 @@ etiquetamiento([H|T],Arbol) :-
 	generar_aristas(Result,T,Lista_nodo1,Lista_arist,_,_,X,Arist),
 	Arbol = nodo(X,Arist).
 
+%------------------------------------------------------------------------------%
+
+%%
+%%
 generar_aristas(N,_,Lista_nodo,Lista_arist,LN,LA,_,[]):- 
 	integer(N), 
 	N==0,
 	Lista_nodo = LN,
 	Lista_arist = LA,!.
 
+%%
+%%
 generar_aristas(_,[],Lista_nodo,Lista_arist,LN,LA,_,[]):- 
 	Lista_nodo = LN,
 	Lista_arist = LA,!.
 
+%%
+%%
 generar_aristas(_,[[]],Lista_nodo,Lista_arist,LN,LA,_,[]):- 
 	Lista_nodo = LN,
 	Lista_arist = LA,!.
 	
+%%
+%%
 generar_aristas(N,Esqueleto,Lista_nodo,Lista_arist,LN,LA,N_nod,Arist) :-
 	integer(N),
 	N > 0,
@@ -220,14 +333,30 @@ generar_aristas(N,Esqueleto,Lista_nodo,Lista_arist,LN,LA,N_nod,Arist) :-
 	LA = LA2,
 	append([arista(Y,nodo(X,Arist1))],Arist2,Arist).
 
+%------------------------------------------------------------------------------%
 
+%%
+%%
 g_drop(_,[],[],[]).
+
+%%
+%%
 g_drop(N,Lista,Tail,Drop):-
 	[H|Tail] = Lista,
 	drop(N,H,Drop),!.
 
+%------------------------------------------------------------------------------%
+
+%%
+%%
 drop(_,[],[]).
+
+%%
+%%
 drop(N,Lista,Lista) :- integer(N), N == 0,!.
+
+%%
+%%
 drop(N,Lista,Tail) :-
 	integer(N),
 	N1 is N-1,
@@ -235,24 +364,31 @@ drop(N,Lista,Tail) :-
 	drop(N1,T,Tail2),!,
 	Tail = Tail2.
 
+%------------------------------------------------------------------------------%
+%                            ESQ_ETIQUETABLE                                   %
+%------------------------------------------------------------------------------%
 
-%---------------------------------------------------------
-%                Predicado es_etiquetable
-%---------------------------------------------------------
-
+%%
+%%
 esqEtiquetable(0,R):- integer(R), R>=0.
+
+%%
+%%
 esqEtiquetable(N,R) :-
 	integer(N),
 	integer(R),
 	forall((esqueleto(N,R,E),etiquetamiento(E,A)),bienEtiquetado(A)).
 
-
 %------------------------------------------------------------------------------%
-%                            DESCRIBIR ETIQUETAMIENTO                          %
+%                        DESCRIBIR ETIQUETAMIENTO                              %
 %------------------------------------------------------------------------------%
 
+%%
+%%
 describirEtiquetamiento(Arbol) :- describirEtiquetamiento(0,Arbol).
 
+%%
+%%
 describirEtiquetamiento(Indent,nodo(X,L)) :-
 	nl, tab(Indent),
 	write(nodo),write('('),write(X),
@@ -260,19 +396,28 @@ describirEtiquetamiento(Indent,nodo(X,L)) :-
 	describirEtiquetamiento(NuevaIndent,L),
 	write(')').
 
+%%
+%%
 describirEtiquetamiento(Indent,[]) :-
 	nl, tab(Indent),
 	write('[]').
 
-
+%%
+%%
 describirEtiquetamiento(Indent,[arista(X,nodo(Y,L))|T]) :-
 	nl, tab(Indent),
 	write('['),
 	imprimirArista(Indent,[arista(X,nodo(Y,L))|T]),
 	write(']').
 
+%------------------------------------------------------------------------------%
+
+%%
+%%
 imprimirArista(_,[]).
 
+%%
+%%
 imprimirArista(Indent,[arista(X,nodo(Y,L))|T]) :-
 	write(arista), write('('), write(X),
 	NuevaIndent is Indent + 8,
