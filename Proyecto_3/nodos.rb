a7e3284d742523b6262b658c89f16f42a48d9d68
Pablo Maldonado <prmm95@gmail.com>
2016-06-24 22:32:49 -0400

    se agrego una posible implementacion para singular / uniforme (string). no es buena

--- Proyecto_3/nodos.rb
+++ Proyecto_3/nodos.rb
@@ -17,5 +17,62 @@
 #------------------------------------------------------------------------------#
 
 class Nodo
-	attr_accessor: element
+	attr_accessor :element
 end
+
+#------------------------------------------------------------------------------#
+#                     Definición de los Mutadores                              #
+#------------------------------------------------------------------------------#
+
+=begin
+class Mutador
+end
+
+class Singular < Mutador
+end
+
+class Uniforme < Mutador
+end
+ 
+class Oscuro < Mutador
+end
+=end 
+#------------------------------------------------------------------------------#
+#                        Redefinición de clases                                #
+#------------------------------------------------------------------------------#
+
+class ::String
+	def singular 
+		new = ""
+		self.each_char do |c|
+			if "singular".include?(c)
+				c = c.upcase
+			end
+			new = new + c
+		end
+		return new
+ 	end
+
+ 	def uniforme
+ 		new = ""
+ 		i = 0
+ 		self.each_char do |c|
+ 			if i % 2 == 0 
+ 				c = c.upcase
+ 			end
+ 			new = new + c
+ 			i = i + 1
+ 		end
+ 		return new
+ 	end
+
+end
+
+
+=begin 
+class String
+end 
+
+class Array
+end
+=end
